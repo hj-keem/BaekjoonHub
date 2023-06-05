43731d9ecdc14f70b0691f2fa91486d7142e4eb5
hj-keem <128394219+hj-keem@users.noreply.github.com>
2023-06-05 16:27:36 +0900

    [level 0] Title: 개미 군단, Time: 0.02 ms, Memory: 71.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120837. 개미 군단/개미 군단.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int hp) {
+        int answer = 0;
+
+        while(hp>0){
+            if(hp >= 5){
+                answer += hp / 5; //몫 저장
+                hp = hp % 5; // 나머지 저장
+            }else if(hp >= 3){
+                answer += hp / 3;
+                hp = hp % 3;
+            }else if(hp >= 1){
+                answer += hp/1;
+                hp = hp % 1;
+            }
+        }
+                return answer;  
+        }
+    }
