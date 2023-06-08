23a1a1801e4e992072106625a13d05067e590052
hj-keem <128394219+hj-keem@users.noreply.github.com>
2023-06-08 17:48:12 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.02 ms, Memory: 70.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int pizza=1;
+        while(true){
+            if(pizza*6 % n ==0){
+                answer=pizza;
+                break;
+            }
+            pizza++;
+        }
+        return answer;
+    }
+  }
